fded719760d525bc1cee71d3a1d271ab1e8ca34d
PronCan <twojini98@gmail.com>
2023-07-22 22:48:24 +0900

    first

---
+++ request/src/component/Page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className='page_wrap'>
+        <div className='left_wrap'>
+            
+        </div>
+        <div className='right_wrap'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Page
