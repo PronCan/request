60639b0aaf984c11cef844a504e5f664e7499620
PronCan <twojini98@gmail.com>
2023-07-28 06:27:36 +0900

    update

--- request/src/component/ViewData.js
+++ request/src/component/ViewData.js
@@ -12,23 +12,37 @@ const ViewData = ({data, date}) => {
     const _data = data
     const dateStr = date  // '2013-12-20' // string type
     const info5 = _data.info5;
+    const info6 = [..._data.info6];
+    console.log(info6)
+    const option = ['선택1', '선택2', '선택3']
 
-    const [radioState, setRadioState] = useState([])
     const [selectedDate, setSelectedDate] = useState(null)
+    const [radioState, setRadioState] = useState([])
+    const [checkboxState, setCheckboxState] = useState([])
 
     useEffect(() => {
         setSelectedDate(convertDate(dateStr))
         setRadioState(info5)
+        setCheckboxState(info6)
     }, [])
+
+    const handleSelectDate = (e) => {
+        // console.log(e);
+        setSelectedDate(e)
+    }
 
     const handleRadioBtn = (e) => {
         // console.log(e.target.value);
         setRadioState(e.target.value)
     }
 
-    const handleSelectDate = (e) => {
-        // console.log(e);
-        setSelectedDate(e)
+    const handleCheckBtn = (e) => {
+        console.log('handleCheckBtn', e.target.value);
+        if(checkboxState.includes(e.target.value)) {
+            setCheckboxState(checkboxState.filter((item) => item !== e.target.value))
+        } else {
+            setCheckboxState([...checkboxState, e.target.value])
+        }
     }
 
     function getInfoData(str) {
@@ -90,7 +104,7 @@ const ViewData = ({data, date}) => {
                     <li>
                         <div className='input-radio input-radio-list'>
                             <label>
-                                <input type='radio' name='info5' value={'선택1'} checked={radioState === '선택1'} onChange={(handleRadioBtn)}/>
+                                <input type='radio' name='info5' value={'선택1'} checked={radioState === '선택1'} onChange={handleRadioBtn}/>
                                 선택1
                             </label>
                             <label>
@@ -110,19 +124,20 @@ const ViewData = ({data, date}) => {
                 <ul>
                     <li>정보6</li>
                     <li>
-                    <div className='input-radio input-radio-list'>
-                            <label>
-                                <input type='checkbox' name='info6' value={'선택1'} checked={radioState === '선택1'} onChange={(handleRadioBtn)}/>
-                                선택1
-                            </label>
-                            <label>
-                                <input type='checkbox' name='info6' value={'선택2'} checked={radioState === '선택2'} onChange={handleRadioBtn}/>
-                                선택2
-                            </label>
-                            <label>
-                                <input type='checkbox' name='info6' value={'선택3'} checked={radioState === '선택3'} onChange={handleRadioBtn}/>
-                                선택3
-                            </label>
+                        <div className='input-checkbox-list'>
+                                <label>
+                                    <input type='checkbox' name='info6' value={'선택1'} checked={0} onChange={() => handleCheckBtn}/>
+                                    선택1
+                                </label>
+                                <label>
+                                    <input type='checkbox' name='info6' value={'선택2'} checked={1} onChange={() => handleCheckBtn}/>
+                                    선택2
+                                </label>
+                            {
+                                option.map((arr) => {
+                                    <p>{arr}</p>
+                                })
+                            }
                         </div>
                     </li>
                 </ul>
